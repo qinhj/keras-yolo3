e5631e2f7fbe09b21c81313ea0a2677f72d746e8
qinhj <qinhongjie@imilab.com>
2021-03-23 19:02:54 +0800

    [scripts] update voc_annotation.py to support both 2007 and 2012

--- scripts/voc_annotation.py
+++ scripts/voc_annotation.py
@@ -1,16 +1,37 @@
+import argparse
 import xml.etree.ElementTree as ET
 from os import getcwd
 
-sets=[('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
-#sets=[('2012', 'train'), ('2012', 'val'), ('2012', 'test')]
-#sets=[('2028', 'train'), ('2028', 'val'), ('2028', 'test')]
+## construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-y", "--year", type=str, default="2007", help="year of VOC datasets")
+ap.add_argument("-c", "--classes_path", type=str, help="path of classes file")
+args = vars(ap.parse_args())
+#print("args: %s" % args)
 
-classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+## VOC datasets: year
+year = args["year"]
+sets = [(year, 'train'), (year, 'val')]
+if "2007" == year:
+    sets.append((year, 'test'))
+print("VOC dataset year: %s, sets: %s" % (year, sets))
 
+def get_classes(classes_path):
+    '''loads the classes'''
+    with open(classes_path) as f:
+        class_names = f.readlines()
+    class_names = [c.strip() for c in class_names]
+    return class_names
+## VOC datasets: classes
+if "classes_path" not in args.keys() or args["classes_path"] is None:
+    classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+else:
+    classes = get_classes(args["classes_path"])
+print("VOC dataset classes: %s" % classes)
 
 def convert_annotation(year, image_id, list_file):
-    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id))
-    tree=ET.parse(in_file)
+    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml' % (year, image_id))
+    tree = ET.parse(in_file)
     root = tree.getroot()
 
     for obj in root.iter('object'):
@@ -26,10 +47,10 @@ def convert_annotation(year, image_id, list_file):
 wd = getcwd()
 
 for year, image_set in sets:
-    image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
-    list_file = open('%s_%s.txt'%(year, image_set), 'w')
+    image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt' % (year, image_set)).read().strip().split()
+    list_file = open('%s_%s.txt' % (year, image_set), 'w')
     for image_id in image_ids:
-        list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg'%(wd, year, image_id))
+        list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg' % (wd, year, image_id))
         convert_annotation(year, image_id, list_file)
         list_file.write('\n')
     list_file.close()
