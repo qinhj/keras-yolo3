e6011efe0d796c0d0f2677c19a905a41d48e6a93
qinhj <qinhongjie@imilab.com>
2021-03-26 04:21:32 +0800

    [srcipts] update voc annotation script to support oxford-iiit-pet dataset

--- scripts/voc_annotation.py
+++ scripts/voc_annotation.py
@@ -49,9 +49,10 @@ def convert_annotation(year, image_id, list_file):
 wd = getcwd()
 
 for year, image_set in sets:
-    image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt' % (year, image_set)).read().strip().split()
+    image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt' % (year, image_set)).readlines()
     list_file = open('%s_%s.txt' % (year, image_set), 'w')
     for image_id in image_ids:
+        image_id = image_id.strip().split()[0]
         list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg' % (wd, year, image_id))
         convert_annotation(year, image_id, list_file)
         list_file.write('\n')
