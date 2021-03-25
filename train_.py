19718a3d698295ade5cd4579a0adb2d42b457ebc
qinhongjie <qinhongjie@imilab.com>
2021-03-26 04:54:56 +0800

    [src] bug fix: to support yolo tiny

--- train_.py
+++ train_.py
@@ -21,31 +21,38 @@ def _main():
     anchors = get_anchors(anchors_path)
 
     input_shape = (416,416) # multiple of 32, hw
-    model = create_model(input_shape, anchors, num_classes)
+
+    is_tiny_version = len(anchors)==6 # default setting
+    if is_tiny_version:
+        model = create_tiny_model(input_shape, anchors, num_classes)
+    else:
+        model = create_model(input_shape, anchors, num_classes)
 
     logging = TensorBoard(log_dir=log_dir)
     checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
-        monitor='val_loss', save_weights_only=True, save_best_only=True, period=1)
+        monitor='val_loss', save_weights_only=True, save_best_only=True, period=3)
 
     val_split = 0.1
     with open(annotation_path) as f:
         lines = f.readlines()
+    np.random.seed(10101)
     np.random.shuffle(lines)
+    np.random.seed(None)
     num_val = int(len(lines)*val_split)
     num_train = len(lines) - num_val
     model.compile(optimizer='adam', loss={
             # use custom yolo_loss Lambda layer.
         'yolo_loss': lambda y_true, y_pred: y_pred})
 
-    batch_size = 10
+    batch_size = 32
     print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
 
     model.fit_generator(
         data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
         steps_per_epoch=max(1, num_train//batch_size),
-        validation_data=data_generator_wrap(lines[num_train:], batch_size, input_shape, anchors, num_classes),
+        validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
         validation_steps=max(1, num_val//batch_size),
-        epochs=500,
+        epochs=100,
         initial_epoch=0)
     model.save_weights(log_dir + 'trained_weights.h5')
  
@@ -92,6 +99,37 @@ def create_model(input_shape, anchors, num_classes, load_pretrained=False, freez
     model = Model([model_body.input, *y_true], model_loss)
 
     return model
+
+def create_tiny_model(input_shape, anchors, num_classes, load_pretrained=False, freeze_body=False,
+            weights_path='model_data/tiny_yolo_weights.h5'):
+    '''create the training model, for Tiny YOLOv3'''
+    K.clear_session() # get a new session
+    image_input = Input(shape=(None, None, 3))
+    h, w = input_shape
+    num_anchors = len(anchors)
+
+    y_true = [Input(shape=(h//{0:32, 1:16}[l], w//{0:32, 1:16}[l], \
+        num_anchors//2, num_classes+5)) for l in range(2)]
+
+    model_body = tiny_yolo_body(image_input, num_anchors//2, num_classes)
+    print('Create Tiny YOLOv3 model with {} anchors and {} classes.'.format(num_anchors, num_classes))
+
+    if load_pretrained:
+        model_body.load_weights(weights_path, by_name=True, skip_mismatch=True)
+        print('Load weights {}.'.format(weights_path))
+        if freeze_body:
+            # Do not freeze 3 output layers.
+            num = len(model_body.layers)-7 # qinhj: check once more!
+            for i in range(num): model_body.layers[i].trainable = False
+            print('Freeze the first {} layers of total {} layers.'.format(num, len(model_body.layers)))
+
+    model_loss = Lambda(yolo_loss, output_shape=(1,), name='yolo_loss',
+        arguments={'anchors': anchors, 'num_classes': num_classes, 'ignore_thresh': 0.7})(
+        [*model_body.output, *y_true])
+    model = Model([model_body.input, *y_true], model_loss)
+
+    return model
+
 def data_generator(annotation_lines, batch_size, input_shape, anchors, num_classes):
     '''data generator for fit_generator'''
     n = len(annotation_lines)
